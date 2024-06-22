0c3be84f78203f8c472953d8d81466b7621e6fff
Alberto <albertosouza16.contato@gmail.com>
2024-06-21 22:17:34 -0300

    consuming api and card styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,27 @@
 import "./globals.sass";
+import { ProductType } from "@/types/ProductType";
+import Product from "./components/Product";
 
-export default function Home() {
+async function getProducts() {
+    const res = await fetch("https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=ASC");
+
+    if (!res.ok){
+      throw new Error('falha ao acessar');
+    }
+
+    return res.json();
+  }
+
+export default async function Home() {
+  const products = await getProducts();
+  console.log(products);
+  
   return (
     <div className="container-grid">
         <div className="cards grid">
-          <div className="card">
-            <h2>Card 1</h2>
-          </div>
-          <div className="card">
-            <h2>Card 1</h2>
-          </div>
-          <div className="card">
-            <h2>Card 1</h2>
-          </div>
-          <div className="card">
-            <h2>Card 1</h2>
-          </div>
-          <div className="card">
-            <h2>Card 1</h2>
-          </div>
-          <div className="card">
-            <h2>Card 1</h2>
-          </div>
-          <div className="card">
-            <h2>Card 1</h2>
-          </div>
-          <div className="card">
-            <h2>Card 1</h2>
-          </div>
+              {products.products.map((product: ProductType) => (
+                <Product key={product.id} product={product}></Product>
+                ))};
         </div>
     </div>
   );
---
+++ src/types/ProductType.ts
@@ -0,0 +1,8 @@
+export type ProductType = {
+    id: number;
+    name: string;
+    brand: string;
+    description: string | null;
+    photo: string;
+    price: number | null;
+}
